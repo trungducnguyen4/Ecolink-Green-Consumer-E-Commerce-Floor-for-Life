828c037cf34ba04bbbbb4c15d0dfa3831b00f22a
trungducnguyen4 <nguyentrungduc.forwork@gmail.com>
2024-10-29 16:19:32 +0700

    mini update

--- routes/routes.js
+++ routes/routes.js
@@ -7,4 +7,16 @@ const fs = require('fs');
 router.get("/", (req, res) => {
     res.render("home", { title: "Home" });
 });
+router.get("/products", (req, res) => {
+    res.render("products", { title: "Products" });
+});
+router.get("/product-detail", (req, res) => {
+    res.render("product-detail", { title: "Products" });
+});
+router.get("/news", (req, res) => {
+    res.render("news", { title: "news" });
+});
+router.get("/forum", (req, res) => {
+    res.render("forum", { title: "Forum" });
+});
 module.exports = router;
